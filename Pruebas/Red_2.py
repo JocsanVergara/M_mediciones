90df05b1abf00fd8c57df777e1b6b5ea1c5a60f6
Jocsan Vergara Blanco <jocsanvergarab@gmail.com>
2022-09-19 04:06:38 -0300

    Pruebas

--- Pruebas/Red_2.py
+++ Pruebas/Red_2.py
@@ -11,8 +11,8 @@ metadatos
 datos_entrenamiento, datos_pruebas= datos['train'], datos['test']
 
 # asignamos los indices a una variables
-nombre_clases = metadatos.features['label'].names
-nombre_clases
+nombres_clases = metadatos.features['label'].names
+nombres_clases
 
 # Normalizar la entrenada de (0-255 a 0-1)
 def normalizar(imagenes, etiquetas):
@@ -51,7 +51,7 @@ for i, (imagen, etiqueta) in enumerate(datos_entrenamiento.take(25)):
     plt.yticks([])
     plt.grid(False)
     plt.imshow(imagen, cmap=plt.cm.binary)
-    plt.xlabel(nombre_clases[etiqueta])
+    plt.xlabel(nombres_clases[etiqueta])
 plt.show()
 
 # Crea el modelo
@@ -86,3 +86,78 @@ historial = modelo.fit(datos_entrenamiento, epochs=5, steps_per_epoch=math.ceil(
 plt.xlabel("# Epoca")
 plt.ylabel("Magnitud de pérdida")
 plt.plot(historial.history["loss"])
+
+#
+import numpy as np
+for imagenes_prueba, etiquetas_prueba in datos_pruebas.take(1):
+    imagenes_prueba = imagenes_prueba.numpy()
+    etiquetas_prueba = etiquetas_prueba.numpy()
+    predicciones = modelo.predict(imagenes_prueba)
+
+def graficar_imagen(i, arr_predicciones, etiquetas_reales, imagenes):
+    arr_predicciones, etiqueta_real, img = arr_predicciones[i], etiquetas_reales[i],imagenes[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img[...,0], cmap = plt.cm.binary)
+
+    etiqueta_prediccion = np.argmax(arr_predicciones)
+    if(etiqueta_prediccion == etiqueta_real):
+        color = 'blue' # Si le acerto a la predicción
+    else:
+        color = 'red'
+  
+    plt.xlabel("{} {:2.0f}% ({})".format(nombres_clases[etiqueta_prediccion],
+                                    100*np.max(arr_predicciones),
+                                    nombres_clases[etiqueta_real]),
+                                    color=color)
+    
+def graficar_valor_arreglo(i, arr_predicciones, etiqueta_real):
+  arr_predicciones, etiqueta_real = arr_predicciones[i], etiqueta_real[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  grafica = plt.bar(range(10), arr_predicciones, color="#777777")
+  plt.ylim([0, 1]) 
+  etiqueta_prediccion = np.argmax(arr_predicciones)
+  
+  grafica[etiqueta_prediccion].set_color('red')
+  grafica[etiqueta_real].set_color('blue')
+  
+filas = 5
+columnas = 5
+num_imagenes = filas*columnas
+plt.figure(figsize=(2*2*columnas, 2*filas))
+for i in range(num_imagenes):
+  plt.subplot(filas, 2*columnas, 2*i+1)
+  graficar_imagen(i, predicciones, etiquetas_prueba, imagenes_prueba)
+  plt.subplot(filas, 2*columnas, 2*i+2)
+  graficar_valor_arreglo(i, predicciones, etiquetas_prueba)
+
+#Probar una imagen suelta
+imagen = imagenes_prueba[4] #AL ser la variable imagenes_prueba solo tiene lo que se le puso en el bloque anterior heheh
+imagen = np.array([imagen])
+prediccion = modelo.predict(imagen)
+
+print("Prediccion: " + nombres_clases[np.argmax(prediccion[0])])
+
+#Exportacion del modelo a h5
+modelo.save('modelo_exportado.h5')
+
+#Instalar tensorflowjs para convertir el h5 a un modelo que pueda cargar tensorflowjs en un explorador
+#!pip install tensorflowjs
+
+#Convertir el archivo h5 a formato de tensorflowjs
+#!mkdir tfjs_target_dir
+#!tensorflowjs_converter --input_format keras modelo_exportado.h5 tfjs_target_dir
+
+#Veamos si si creo la carpeta
+#!ls
+
+#Veamos el contenido de la carpeta
+#!ls tfjs_target_dir
+
+
+
+
