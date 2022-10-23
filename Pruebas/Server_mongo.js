2ef668d1d46f8661acea512442341e394b4fc46a
Jocsan Vergara Blanco <jocsanvergarab@gmail.com>
2022-10-23 02:00:14 -0300

    db

--- Pruebas/Server_mongo.js
+++ Pruebas/Server_mongo.js
@@ -1,18 +1,109 @@
- //Crear un documento
- // Documento -> Objetos de tipo JSON
+//Crear un documento
+// Documento -> Objetos de tipo JSON
 
- user1 ={
+user1 ={
     'username':'user1',
     'age':27,
     'email':'user1@example.com'
  }
 // Se valida que la base de datos exista.
 // Se valida que la colección exitosa.
- db.users.insert(user1)
+db.users.insert(user1)
+
+db.users.find(); // Cursores
+
+// ObjectID ->4
+// 1.- (Timestamp)
+// 2.- (Identificador para el servidor)
+// 3.- (PID)
+// 4.- (AutoIncrement)
 
 
+// Para insertar un documento uso 
+db.users.insertOne(user4);
 
- -- Crear una base de datos
+// Si quiero insertar multiples Documentos puedo usar el comando insertMany
+db.users.insertMany(
+    [
+       {'username':'user2',
+       'age':27,
+       'email':'user2@example.com'},
+       {'username':'user3',
+       'age':27,
+       'email':'user3@example.com'},
+       {'username':'user4',
+       'age':27,
+       'email':'user4@example.com'}, 
+    ]
+);
+
+// Podemos también usar la función save el cual nos permite ingresar un documento a la coleccion
+user5 ={
+    'username':'user8',
+    'age':56,
+    'email':'user8@example.com'
+};
+db.users.save(user5);
+// Si el objeto no existe se crea. (_id)
+// Si el objeto existe se actualiza su valor. (_id)
+
+// Como podemos hacer consultas
+//Obtener el usuario con username user7
+
+// Retorna un cursor
+db.users.find({
+    username : 'user7'
+});
+
+// Retorna un documento
+db.users.findOne({
+    username : 'user7'
+});
+
+// Obtener todos los usuarios con una edad mayor a 10 (>)
+db.users.find({
+    age : {$gt :25}
+});
+
+// Obtener la cantidad de usuarios con una edad menor a 50 (<)
+db.users.find({
+    age : {$lt :50}
+}).count();
+
+// Obtener todos los usuarios cuya edad mayor a 10 y cuyo estatus sea activo
+db.users.find({
+    $and: [
+        {age : {$gt : 10}},
+        {status:'active'}
+    ]
+});
+
+// Obtener todos los usuarios que no tengan la edad de 11
+db.users.find({
+    age : {$ne : 11}
+});
+
+// Obtener todos los usuarios que tengan por edad: 27 o 40 o 11
+db.user.find({
+    $or : [
+        {age : 27},
+        {age : 40},
+        {age : 11}
+    ]
+});
+
+db.user.find({
+    age : {$in : [27, 40, 11]}
+});
+
+// Obtener todos los usuarios con atributo estatus
+// Obtener todos los usuarios con estatus activo
+// Obtener todos los usuarios con estatus activo y correo electrónico
+// Obtener el usuario con mayor edad
+// Obtener a los tres usuarios más jovenes
+
+
+// Crear una base de datos
 use Nombre
 db
 show dbs
